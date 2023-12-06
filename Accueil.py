6d57952a6b63f644e451cb3ce3870f2a7b7a2a85
adriana zokra <adi.zokra@gmail.com>
2023-12-06 02:08:45 +0100

    logo resources

--- Accueil.py
+++ Accueil.py
@@ -4,11 +4,21 @@ import streamlit as st
 import dvf_concat_trait
 from PIL import Image
 import datetime
+import os
+
+# Get the current directory of the Python script
+script_dir = os.path.dirname(os.path.abspath(__file__))
+
+# Define the relative path to the resources folder
+resources_dir = os.path.join(script_dir, "resources")
+
+# Access a specific resource within the "resources" folder
+logo_path = os.path.join(resources_dir,"logo.jpg")
 
 
 #Adding logo
 #st.set_page_config(layout="wide")
-image = Image.open('resources/logo.jpg')
+image = Image.open(logo_path)
 col1,col2 = st.columns([0.05,0.95])
 
 #Insertion de notre logo
